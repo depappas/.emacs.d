3b35613bf7112d4501017942a05d3328337eb7ed
Derek Pappas <depappas@yahoo.com>
2014-04-20 21:57:57 +0200

    .

---
+++ python_setup.el
@@ -0,0 +1,48 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; name the package
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provides python_setup)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://github.com/kiwanami/emacs-epc
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'epc)
+(require 'jedi)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Jedi setup
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;Jedi
+(autoload 'jedi:setup "jedi" nil t)
+(add-hook 'python-mode-hook
+(lambda ()
+(jedi:setup)
+))
+
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
+
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:server-command (list "python3" jedi:server-script))
+;; (jedi-mode 1)
+;; 
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; ;;(add-hook 'python-mode-hook 'jedi:ac-setup)
+;; (setq jedi:setup-keys t)
+;; (setq jedi:server-args
+;;       '("--log-level" "DEBUG"
+;;         "--log-traceback"))
+;; (setq jedi:complete-on-dot t)
+;; (autoload 'jedi:setup "jedi" nil t)
+;; 
+;; 
+;; 
+;; (defun my-python-hook ()
+;;   (interactive)
+;;   (define-key python-mode-map (kbd "RET") 'newline-and-indent))
+;; (add-hook 'python-mode-hook 'my-python-hook)
+;; 
+;; (setq jedi:setup-keys t)
